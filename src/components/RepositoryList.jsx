32f11875d25cd92bbb29b8fa055b5a673257c76a
J-DouglasLira <jdouglira@gmail.com>
2022-03-15 10:01:48 -0300

    repository-list added

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,43 @@
+
+const repositoryName = 'unform2';
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositório</h1>
+
+      <ul>
+        <li>
+          <strong> {repositoryName} </strong>
+          <p>Forms in react</p>
+          <a href="">acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="">acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="">acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="">acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Forms in react</p>
+          <a href="">acessar repositório</a>
+        </li>
+
+      </ul>
+    </section>
+  );
+}
