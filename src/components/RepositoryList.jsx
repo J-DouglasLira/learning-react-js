a22853a9a133a1c80ac11c58fa0a0cd971783ee3
J-DouglasLira <jdouglira@gmail.com>
2022-03-16 10:13:24 -0300

    repositories scss added

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,12 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositório</h1>
-
       <ul>
-       
        <RepositoryItem repository='unform2'/>
        <RepositoryItem/>
        <RepositoryItem/>
